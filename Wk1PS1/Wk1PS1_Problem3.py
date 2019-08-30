74d83501efa8e622b8e0b798d2cbb12440f7eac9
godprobe <godprobe.misc+git@gmail.com>
2019-08-30 14:43:45 -0700

    Fixed bug in the case where the first letter was the best result.

--- Wk1PS1/Wk1PS1_Problem3.py
+++ Wk1PS1/Wk1PS1_Problem3.py
@@ -1,9 +1,10 @@
-s = 'abcdefghijklmnopqaidtheresneveranotherproblemsherwoodgabcdrapefruyzitoccurrencefghjlmnopqrsssstuyvz'
-longest = 0
-current = ''
-best = ''
-for letter in s:
-	if current.isalpha() and letter >= current[-1]:
+# NOTE: This could be more robust: results in an error if s is a blank string.
+s = 'z'
+longest = 1
+current = s[0]
+best = current
+for letter in s[1:]:
+	if letter >= current[-1]:
 		current += letter
 		# print("Current eval:",current)
 		if len(current) > longest:
