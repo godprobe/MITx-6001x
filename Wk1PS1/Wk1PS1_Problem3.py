eec17639368d9de7143794a55802a49a45ff7753
godprobe <godprobe.misc+gitcommit@gmail.com>
2019-08-30 13:33:07 -0700

    Wk1PS1 Problem 3 skeleton added

---
+++ Wk1PS1/Wk1PS1_Problem3.py
@@ -0,0 +1,7 @@
+s = 'abaidtheresneveranotherproblemsherwoodgrapefruitoccurrence'
+	# get curLetter
+	# if next letter is alpha-after
+	# then keep going
+	# otherwise record the length, if it's longer than earlier length
+	# and get a new next letter
+	# until reaching the end of the string
